e3538b8adee9cde2dbea5053cdcc9a68b6ebd2a3
calamite-git <elarmandoesparza@gmail.com>
2025-01-14 20:48:39 -0600

    A: Start of project

---
+++ main.py
@@ -0,0 +1,16 @@
+# Allows use of code from
+# the open-source pygame library
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+
+
+if __name__ == "__main__":
+        main()
+
