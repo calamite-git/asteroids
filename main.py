1af924bf518c33d92d7a1488f475469d80393964
calamite-git <elarmandoesparza@gmail.com>
2025-01-22 12:15:33 -0600

    B:Setup game loop

--- main.py
+++ main.py
@@ -3,11 +3,23 @@
 import pygame
 from constants import *
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
 def main():
+    running = True
+    pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill("black")
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
 
 
 
