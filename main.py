f01a399c933d07e984c11b1721bf620df4996e6e
calamite-git <elarmandoesparza@gmail.com>
2025-01-27 17:56:20 -0600

    C: Create player and asteroid with collisions working

--- main.py
+++ main.py
@@ -1,28 +1,54 @@
-# Allows use of code from
-# the open-source pygame library
+import sys
 import pygame
 from constants import *
+from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
+
 def main():
-    running = True
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-    while running:
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-        screen.fill("black")
-        pygame.display.flip()
-        clock.tick(60)
-    pygame.quit()
+                return
 
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over!")
+                sys.exit()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
-        main()
-
+    main()
